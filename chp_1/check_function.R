57d66f9bb93f321736a12a08d113befa8df79f4a
iadewitz <64585782+iadewitz@users.noreply.github.com>
2026-04-30 12:10:57 +0200

    minor changes

--- chp_1/check_function.R
+++ chp_1/check_function.R
@@ -16,7 +16,7 @@ alpha = 0.25
 y_0.25 = sapply(u, function(u) check_loss(u, alpha = alpha))
 out = cbind(u, y_0.25)
 write.table(out,
-    file = "main/chapter_1/pictures_chapter_1/check_0_25.dat",
+    file = "output/plots/chp_1/check_0_25.dat",
     col.names = FALSE,
     row.names = FALSE
 )
@@ -26,7 +26,7 @@ alpha = 0.5
 y_0.5 = sapply(u, function(u) check_loss(u, alpha = alpha))
 out = cbind(u, y_0.5)
 write.table(out,
-    file = "main/chapter_1/pictures_chapter_1/check_0_50.dat",
+    file = "output/plots/chp_1/check_0_50.dat",
     col.names = FALSE,
     row.names = FALSE
 )
@@ -36,7 +36,7 @@ alpha = 0.75
 y_0.75 = sapply(u, function(u) check_loss(u, alpha = alpha))
 out = cbind(u, y_0.75)
 write.table(out,
-    file = "main/chapter_1/pictures_chapter_1/check_0_75.dat",
+    file = "output/plots/chp_1/check_0_75.dat",
     col.names = FALSE,
     row.names = FALSE
 )
